b6ca6ea0e752f0cde02269ffaea8767469de6ba8
Omar Ayman <omaralqtany81@gmail.com>
2026-05-16 00:51:42 +0300

    Delete scratch directory

--- scratch/Test.java
+++
@@ -1,9 +0,0 @@
-import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
-public class Test {
-    public static void main(String[] args) {
-        MessageCreateBuilder B = new MessageCreateBuilder();
-        // b.useComponentsV2(true);
-    }
-}
-public class Test2 {}
-
