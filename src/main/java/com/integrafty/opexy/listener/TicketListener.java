7015b75284718e58dedef6f3fb8c8b32dff22740
Omarayman008 <omarayman008@gmail.com>
2026-06-30 19:09:07 +0300

    Initial Bot Setup

--- src/main/java/com/integrafty/opexy/listener/TicketListener.java
+++ src/main/java/com/integrafty/opexy/listener/TicketListener.java
@@ -694,7 +694,7 @@ public class TicketListener extends ListenerAdapter {
         event.deferReply(true).queue();
         
         ticketRepository.findByChannelId(channel.getId()).ifPresentOrElse(ticket -> {
-            String domain = "https://highcoremc-production.up.railway.app";
+            String domain = "https://dc-bot-production-e2d3.up.railway.app";
             String link = domain + "/view/transcript/" + ticket.getId();
             
             // Send to user ephemerally
